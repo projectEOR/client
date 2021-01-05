4a591ca7d36523c24122cad167abbf669728b42b
jrconsole <jrconsole@gmail.com>
2021-01-05 14:27:01 -0800

    fix router test

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,14 +1,25 @@
 import App from './App';
+import Tracker from '../Tracker/Tracker';
+import Builder from '../Builder/Builder';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router';
 
+//const rrd = require('react-router-dom');
+//jest.spyOn(rrd, 'BrowserRouter').mockImplementation(({children}) => children);
+
+const rrd = require('react-router-dom');
+// Just render plain div with its children
+rrd.BrowserRouter = ({children}) => <div>{children}</div>
+module.exports = rrd;
+
 test('valid path should render correct component', () => {
   const wrapper = mount(
-    <MemoryRouter initialEntries={[ '/Organization' ]}>
+    <MemoryRouter initialEntries={[ '/tracker' ]}>
       <App/>
     </MemoryRouter>
   );
-  expect(wrapper.find(Organization)).toHaveLength(1);
+
+  expect(wrapper.find(Tracker)).toHaveLength(1);
 });
