e4a412313fdea6ce36cf4f8bf2dd5f05ed963b3f
J Silva <72325372+silvasaurus@users.noreply.github.com>
2021-01-12 11:36:55 -0500

    Tracker Table is visible. next step is to update assinged and action

--- src/Components/Tracker/Tracker.js
+++ src/Components/Tracker/Tracker.js
@@ -13,32 +13,37 @@ class Tracker extends Component {
   }
 
   componentDidMount = async () => {
-    const response = await fetch(`/rater/2`);
-    const json = await response.json();
-    this.setState({ data: json });
+    try {
+      console.log("WTF ONE");
+      const response = await fetch(`http://localhost:4000/tracker`);
+      const json = await response.json();
+      this.setState({ data: json });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  handleNewTrack = async () => {
-    const reqOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        closeout: "2021-11-30",
-        sq_suspense: "2021-11-10",
-        gp_suspense: "2021-11-17",
-        wg_suspense: "2021-11-24",
-        member_role: "rater",
-        ratee_id: 4,
-        rater_id: 2,
-        action_id: 1,
-        org_name_id: 2,
-      }),
-    };
+  // handleNewTrack = async () => {
+  //   const reqOptions = {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       closeout: "2021-11-30",
+  //       sq_suspense: "2021-11-10",
+  //       gp_suspense: "2021-11-17",
+  //       wg_suspense: "2021-11-24",
+  //       member_role: "rater",
+  //       ratee_id: 4,
+  //       rater_id: 2,
+  //       action_id: 1,
+  //       org_name_id: 2,
+  //     }),
+  //   };
 
-    const response = await fetch(`/tracker`, reqOptions);
+  //   const response = await fetch(`/tracker`, reqOptions);
 
-    this.setState({ data: response });
-  };
+  //   this.setState({ data: response });
+  // };
 
   handleUpdateAction = (event, id) => {
     event.preventDefault();
@@ -54,7 +59,7 @@ class Tracker extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="Tracker">
         <Table click={this.handleUpdateAction} data={this.state.data} />
 
         <Form report={this.state.activeReport} click={this.handleNewTrack} />
