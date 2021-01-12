383c1e0de987b3b9f88e24a2d89940d26b3d2466
J Silva <72325372+silvasaurus@users.noreply.github.com>
2021-01-12 17:21:45 -0500

    PUT works, but with errors.

--- src/Components/Tracker/Form/Form.js
+++ src/Components/Tracker/Form/Form.js
@@ -2,32 +2,69 @@ import React from "react";
 import "./Form.css";
 
 const form = (props) => {
+  const updateReport = (
+    <form onSubmit={(event) => props.click(event)}>
+      <label className="Options">
+        Update Action :
+        <select
+          className="Options"
+          value={props.action}
+          onChange={props.actionFunc}
+        >
+          <option value="1">Draft</option>
+          <option value="2">Review</option>
+          <option value="3">Edit</option>
+          <option value="4">Sign</option>
+        </select>
+      </label>
+      <br></br>
+      <br></br>
+      <label className="Options">
+        Update Assigned :
+        <select
+          className="Options"
+          value={props.assigned}
+          onChange={props.assignedFunc}
+        >
+          <option value="Rater">Rater</option>
+          <option value="A-Rater">A-Rater</option>
+          <option value="Admin">Admin</option>
+          <option value="Sq/CC">Sq/CC</option>
+          <option value="Gp/CC">Gp/CC</option>
+          <option value="Wg/CC">Wg/CC</option>
+        </select>
+      </label>
+      <br></br>
+      <br></br>
+      <button className="Options" onClick={props.click}>
+        Submit
+      </button>
+    </form>
+  );
+
   return (
     <div className="Form">
       <div>
         {props.report ? (
-          <p className="ratee">
+          <p>
             <b>Close Out:</b> {props.report.closeout.slice(0, 10)}
           </p>
         ) : (
-          <h3>Select Report to Update</h3>
+          <h3> Select Report to Update</h3>
         )}
         {props.report ? (
-          <p className="ratee">
+          <p>
             <b>Ratee:</b> {props.report.ratee_id}
           </p>
         ) : null}
         {props.report ? (
-          <p className="ratee">
+          <p>
             <b>Assigned To:</b> {props.report.member_role}
           </p>
         ) : null}
       </div>
 
-      <form onSubmit={(event) => props.click(event)}>
-        <input type="text" />
-        <button>Update Tracker</button>
-      </form>
+      {props.report ? updateReport : null}
     </div>
   );
 };
--- src/Components/Tracker/Tracker.js
+++ src/Components/Tracker/Tracker.js
@@ -9,43 +9,56 @@ class Tracker extends Component {
     this.state = {
       data: [{}],
       activeReport: "",
+      updateAction: "",
+      updateAssignedTo: "",
     };
   }
 
   componentDidMount = async () => {
     try {
-      console.log("WTF ONE");
       const response = await fetch(`http://localhost:4000/tracker`);
       const json = await response.json();
       this.setState({ data: json });
     } catch (e) {
-      console.log(e);
+      console.error(e);
     }
   };
 
-  // handleNewTrack = async () => {
-  //   const reqOptions = {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({
-  //       closeout: "2021-11-30",
-  //       sq_suspense: "2021-11-10",
-  //       gp_suspense: "2021-11-17",
-  //       wg_suspense: "2021-11-24",
-  //       member_role: "rater",
-  //       ratee_id: 4,
-  //       rater_id: 2,
-  //       action_id: 1,
-  //       org_name_id: 2,
-  //     }),
-  //   };
+  handleUpdateRouting = async (event) => {
+    event.preventDefault();
 
-  //   const response = await fetch(`/tracker`, reqOptions);
+    const reqOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        member_role: this.state.updateAssignedTo,
+        action_id: this.state.updateAction,
+      }),
+    };
 
-  //   this.setState({ data: response });
-  // };
+    const response = await fetch(
+      `http://localhost:4000/tracker/ratee/${this.state.activeReport.ratee_id}`,
+      reqOptions
+    );
 
-  handleUpdateAction = (event, id) => {
+    this.setState({ data: response });
+  };
+
+  handleActionRouting = (event) => {
+    event.preventDefault();
+    this.setState({
+      updateAction: event.target.value,
+    });
+  };
+
+  handleAssignedRouting = (event) => {
+    event.preventDefault();
+    this.setState({
+      updateAssignedTo: event.target.value,
+    });
+  };
+
+  handleSelectReport = (event, id) => {
     event.preventDefault();
 
     let thisOne = this.state.data.filter(
@@ -60,9 +73,16 @@ class Tracker extends Component {
   render() {
     return (
       <div className="Tracker">
-        <Table click={this.handleUpdateAction} data={this.state.data} />
+        <Table click={this.handleSelectReport} data={this.state.data} />
 
-        <Form report={this.state.activeReport} click={this.handleNewTrack} />
+        <Form
+          report={this.state.activeReport}
+          click={this.handleUpdateRouting}
+          actionFunc={this.handleActionRouting}
+          action={this.state.updateAction}
+          assignedFunc={this.handleAssignedRouting}
+          assigned={this.state.updateAssignedTo}
+        />
       </div>
     );
   }
